4c56499097eabf86e4abced22b707905a15e032e
ujjwal7983 <ujjwalsoni7983@gmail.com>
2026-06-21 01:52:41 +0530

    Update CORS origin for production deployment

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookiesParser());
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://learnsphere-1-xmil.onrender.com",
     credentials: true,
 }))
 
