932e18702d4e3739dd7a2ee2d1bd8ac93c7c15f4
ujjwal7983 <ujjwalsoni7983@gmail.com>
2026-06-21 19:21:57 +0530

    Enhance getCurrentUser hook with loading state
    Added loading state management to user fetching.

--- frontend/src/customHooks/getCurrentUser.js
+++ frontend/src/customHooks/getCurrentUser.js
@@ -2,23 +2,33 @@ import React, { useEffect } from "react";
 import axios from "axios";
 import { serverUrl } from "../App";
 import { useDispatch } from "react-redux";
-import { setUserData } from "../redux/userSlice";
+import { setUserData, setLoading } from "../redux/userSlice";
 
 
 const getCurrentUser = () => {
     const dispatch = useDispatch();
-   useEffect(() =>{
-     const fetchUser = async () => {
-        try {
-            const result = await axios.get(serverUrl + "/api/user/getcurrentuser", {withCredentials: true})
-            dispatch(setUserData(result.data))
-        } catch (error) {
-            console.log(error);
-            dispatch(setUserData(null))
-        }
-     }
-     fetchUser();
-   }, [])    
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            try {
+                const result = await axios.get(
+                    serverUrl + "/api/user/getcurrentuser",
+                    { withCredentials: true }
+                );
+
+                dispatch(setUserData(result.data));
+
+            } catch (error) {
+                console.log(error);
+                dispatch(setUserData(null));
+
+            } finally {
+                dispatch(setLoading(false));
+            }
+        };
+
+        fetchUser();
+    }, []);
 }
 
 export default getCurrentUser;
