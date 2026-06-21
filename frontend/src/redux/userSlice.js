aa5102d181229bba1df9c534c6eaa97bbc22a5de
ujjwal7983 <ujjwalsoni7983@gmail.com>
2026-06-21 19:20:24 +0530

    Add loading state to userSlice

--- frontend/src/redux/userSlice.js
+++ frontend/src/redux/userSlice.js
@@ -3,14 +3,19 @@ import { createSlice } from '@reduxjs/toolkit';
 const userSlice = createSlice({
     name: "user",
     initialState: {
-        userData: null
+        userData: null,
+        loading: true
     },
     reducers:{
-        setUserData: (state, action) => {
-            state.userData = action.payload;
-        }
+    setUserData: (state, action) => {
+        state.userData = action.payload;
+    },
+
+    setLoading: (state, action) => {
+        state.loading = action.payload;
     }
+}
 })
 
-export const { setUserData } = userSlice.actions;
+export const { setUserData, setLoading } = userSlice.actions;
 export default userSlice.reducer;
